d84d6b78d33ca7198d398c7d962ed437b476d4c6
Illia Leedskiy <leedqvo@gmail.com>
2023-05-08 17:51:08 +0200

    add block5 css

--- js/scriptP1.js
+++ js/scriptP1.js
@@ -1,6 +1,7 @@
 const elem = document.documentElement;
 let elemWidth = elem.clientWidth;
 const block1Text = document.querySelector('.block1__text');
+const block5Paragraph = document.querySelector('.block5__paragraph');
 
 window.addEventListener("resize", windowSizeChange);
 
@@ -12,12 +13,16 @@ function windowSizeChange() {
         <div class="block1__separator"></div>
         <h3 class="block1__phrase1">Even knights<br/>can't resist pets</h3>
         <h3 class="block1__phrase2">Shelter a new<br/>friend in<br/>your castle</h3>`;
+        block5Paragraph.innerHTML = `There are many others<br/> which also need your help. <br/>Here you can find some of
+        them:`;
     }
     else {
         block1Text.innerHTML = `<h1 class="block1__name">KNIGHT'S SOFT SPOT</h1>
         <div class="block1__separator"></div>
         <h3 class="block1__phrase1">Even knights can't resist pets</h3>
         <h3 class="block1__phrase2">Shelter a new friend in your castle</h3>`;
+        block5Paragraph.innerHTML = `There are many others which also need your help. <br/>Here you can find some of
+        them:`;
     }
 }
 
